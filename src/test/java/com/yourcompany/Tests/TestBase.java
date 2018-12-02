f550c5c3d893505c0967052958844a69ba35b9bb
cproint <cproint@gmail.com>
2018-12-02 00:16:52 -0800

    minor enhancement to TestBase.java

--- src/test/java/com/yourcompany/Tests/TestBase.java
+++ src/test/java/com/yourcompany/Tests/TestBase.java
@@ -104,6 +104,12 @@ public class TestBase  {
         // set current sessionId
         String id = ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
         sessionId.set(id);
+        
+   	 
+        String message = String.format("SauceOnDemandSessionID=%1$s job-name=%2$s",
+        (((RemoteWebDriver) getWebDriver()).getSessionId().toString()), methodName.getClass().getName());
+        System.out.println(message);
+        
     }
 
     /**
