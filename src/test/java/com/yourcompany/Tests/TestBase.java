5054decf38c510070e4576748b54c161eeba7bb0
adoptiveinsight <adoptiveinsight@gmail.com>
2019-01-02 14:53:49 -0800

    updated TestBase

--- src/test/java/com/yourcompany/Tests/TestBase.java
+++ src/test/java/com/yourcompany/Tests/TestBase.java
@@ -24,14 +24,14 @@ public class TestBase  {
 
     public String buildTag = System.getenv("BUILD_TAG");
 
-/*    public String username = System.getenv("SAUCE_USERNAME");
+    public String username = System.getenv("SAUCE_USERNAME");
 
-    public String accesskey = System.getenv("SAUCE_ACCESS_KEY");*/
+    public String accesskey = System.getenv("SAUCE_ACCESS_KEY");
     
 
-    public String username = "muralitulugu";
+/*    public String username = "muralitulugu";
 
-    public String accesskey = "a82b8eed-57dd-43f1-a10e-496ebdf2f3e8";
+    public String accesskey = "a82b8eed-57dd-43f1-a10e-496ebdf2f3e8";*/
 
     /**
      * ThreadLocal variable which contains the  {@link WebDriver} instance which is used to perform browser interactions with.
